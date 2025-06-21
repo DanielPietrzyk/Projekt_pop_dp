dfafa3debf5ead70b74dd8574efec7f50dc9aa9f
DanielPietrzyk <danielpietrzyk07@gmail.com>
2025-06-21 20:01:28 +0200

    wip

--- main.py
+++ main.py
@@ -68,3 +68,58 @@ class Klient:
             return [latitude, longitude]
         except:
             return [52.23, 21.00]
+
+# GUI
+root = Tk()
+root.title('System Zarządzania Urzędami')
+root.geometry('1000x800')
+
+# Ramki
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektow = Frame(root)
+ramka_mapa = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0, padx=50, sticky="n")
+ramka_formularz.grid(row=0, column=1, sticky="n")
+ramka_szczegoly_obiektow.grid(row=1, column=0, columnspan=2)
+ramka_mapa.grid(row=2, column=0, columnspan=2)
+
+# Ramka lista obiektów
+label_lista_obiektow = Label(ramka_lista_obiektow, text='Lista obiektów')
+label_lista_obiektow.grid(row=0, column=0, columnspan=3)
+
+listbox_lista_obiektow = Listbox(ramka_lista_obiektow, width=50)
+listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
+
+button_pokaz_szczegoly = Button(ramka_lista_obiektow, text='Pokaż szczegóły')
+button_pokaz_szczegoly.grid(row=2, column=0)
+
+button_usun_obiekt = Button(ramka_lista_obiektow, text='Usuń obiekt')
+button_usun_obiekt.grid(row=2, column=1)
+
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text='Edytuj obiekt')
+button_edytuj_obiekt.grid(row=2, column=2)
+
+# Ramka formularz
+label_formularz = Label(ramka_formularz, text='Formularz')
+label_formularz.grid(row=0, column=0, columnspan=2)
+
+# Formularz urzędów
+label_nazwa_urzad = Label(ramka_formularz, text='Nazwa urzędu')
+label_nazwa_urzad.grid(row=1, column=0, sticky=W)
+entry_nazwa_urzad = Entry(ramka_formularz)
+entry_nazwa_urzad.grid(row=1, column=1)
+
+label_adres_urzad = Label(ramka_formularz, text='Adres')
+label_adres_urzad.grid(row=2, column=0, sticky=W)
+entry_adres_urzad = Entry(ramka_formularz)
+entry_adres_urzad.grid(row=2, column=1)
+
+label_miejscowosc_urzad = Label(ramka_formularz, text='Miejscowość')
+label_miejscowosc_urzad.grid(row=3, column=0, sticky=W)
+entry_miejscowosc_urzad = Entry(ramka_formularz)
+entry_miejscowosc_urzad.grid(row=3, column=1)
+
+button_dodaj_urzad = Button(ramka_formularz, text='Dodaj', command=dodaj_urzad)
+button_dodaj_urzad.grid(row=4, column=0, columnspan=2)
